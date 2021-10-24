0c5a6cfaac3f3bf4a9cfeb394255666381fbbd5f
ThanhLam <luuthanhlam.c111@gmail.com>
2021-10-24 13:36:47 +0700

    Add project files.

---
+++ PropertyWeb_Team8/Areas/Admin/Controllers/PropertyAdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PropertyWeb_Team8.Models;
+
+namespace PropertyWeb_Team8.Areas.Admin.Controllers
+{
+    public class PropertyAdminController : Controller
+    {
+        PropertyWebEntities model = new PropertyWebEntities();
+        // GET: Admin/PropertyAdmin
+        public ActionResult Index()
+        {
+            var property = model.Properties.OrderByDescending(x => x.ID).ToList();
+            return View(property);
+        }
+    }
+}
